d943592cc30d0d725a718a86776c8c9c00982090
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-16 07:07:20 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long: %zu byte(s)\n", sizeof(long));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(double));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long double));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
