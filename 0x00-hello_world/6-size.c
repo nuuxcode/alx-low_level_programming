15032026ad1bad8f843535347ec3568ed65544ab
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-16 07:05:30 -0700

    up

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(double));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long double));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
