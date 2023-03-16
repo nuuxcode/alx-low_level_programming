8e9eeb6cbe750a5e8ebc1d67a7e7dd6cee134a4f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-16 07:51:22 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
 	write(1, str, 58);
 	return (1);
