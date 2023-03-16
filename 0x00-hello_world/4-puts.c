01a0938a99e3c1203a2d7d7db25b04dd77112e49
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-16 06:42:40 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    char *message = "\"Programming is like building a multilingual puzzle";
-    puts(message);
-    return (0);
+	char *message = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
 }
