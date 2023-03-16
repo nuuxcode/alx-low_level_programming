4fcba15f708219acab48510f9aebe300e27aeaac
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-16 06:48:45 -0700

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "ith proper grammar, but the outcome is a piece of art,";
+
+	printf("%s",message);
+	return (0);
+}
