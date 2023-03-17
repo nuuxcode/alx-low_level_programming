8681d2fe82acdf0c6478b6f8db9656ad1eb153b5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 00:15:27 +0100

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	for (str = 'A'; str <= 'Z'; str++)
+	{
+		putchar(str);
+	}
+	putchar('\n');
+	return (0);
+}
