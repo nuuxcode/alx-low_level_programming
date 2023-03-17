6499d9fa1d66fcc817d0a19b194979876db33cfe
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 00:22:28 +0100

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		printf("%d",n);
+	}
+	printf("\n");
+	return (0);
+}
