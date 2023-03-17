b2a8e7124c695214f2da619f1b84f2caf0be321a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 00:29:48 +0100

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	printf("\n");
+	return (0);
+}
