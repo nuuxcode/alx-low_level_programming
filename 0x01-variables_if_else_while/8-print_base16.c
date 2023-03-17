88d8b20df29527294acd801f9f096bb97b59c21d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 00:56:50 +0100

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (int i = 0; i < 16; i++)
+    {
+        if (i < 10)
+        {
+            putchar(i + '0');
+        }
+        else
+        {
+            putchar(i - 10 + 'a');
+        }
+    }
+    putchar('\n');
+    return (0);
+}
