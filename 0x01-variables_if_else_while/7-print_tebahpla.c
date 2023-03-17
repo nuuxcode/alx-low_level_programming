4e084c343a908e5bd9bd96a1071ad3728b1093b4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 00:47:45 +0100

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str;
+
+	for (str = 'z'; str >= 'a'; str--)
+	{
+		putchar(str);
+	}
+	putchar('\n');
+	return (0);
+}
