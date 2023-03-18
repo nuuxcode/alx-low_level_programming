028ab85623354ddb47fc54e4853059d3178d6b79
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 01:39:58 +0100

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <= 8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a < 7 || b < 8 || c < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
