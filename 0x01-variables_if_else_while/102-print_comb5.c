a1dcb45b3fdf44cf62fe64188440e00d0d4d702e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 01:56:56 +0100

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++) {
-		for (j = i + 1; j <= 99; j++) {
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if (i < 98 || j < 99) {
+			if (i < 98 || j < 99)
+			{
 				putchar(',');
 				putchar(' ');
 			}
