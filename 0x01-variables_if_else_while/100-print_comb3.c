b6d2c8deb7354b6123c3da3b74efb78906b09093
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 01:33:26 +0100

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a, b;
+
+    for (a = 0; a <= 8; a++)
+    {
+        for (b = a + 1; b <= 9; b++)
+        {
+            putchar(a + '0');
+            putchar(b + '0');
+            if (a < 8)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,14 @@
 int main(void)
 {
 	int a, b;
-
-	for (a = 0; a <= 8; a++)
+	
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		putchar(a);
+		if (a != 57)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			if (a < 8)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
