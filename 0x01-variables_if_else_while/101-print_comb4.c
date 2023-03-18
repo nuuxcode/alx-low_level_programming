d8d3a0c1719a1192376f31a1881764a79fae1854
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 01:54:56 +0100

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,25 @@
  */
 int main(void)
 {
-	int i, j;
+	int a, b, c;
 
-    for (i = 0; i < 99; i++) {
-        for (j = i + 1; j <= 99; j++) {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-            if (i < 98 || j < 99) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <= 8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a < 7 || b < 8 || c < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+    for (i = 0; i < 99; i++) {
+        for (j = i + 1; j <= 99; j++) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i < 98 || j < 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+	putchar('\n');
+	return (0);
+}
