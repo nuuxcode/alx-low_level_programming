3dcd2b879ba0d54c6facf90ea7055d45bff099e5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-18 01:02:37 +0100

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-        {
-            putchar(i + '0');
-        }
-        else
-        {
-            putchar(i - 10 + 'a');
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
