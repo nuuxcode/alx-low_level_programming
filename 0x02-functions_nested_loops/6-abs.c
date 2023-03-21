e6a0acc90c9402c1eca5b5887074805567035357
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 20:13:50 +0000

    updated

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - dqzdqzdqzdqzd
+ * @n: Description of parameter x
+ *
+ * Description: ddzqdqzd
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	return (n);
+}
