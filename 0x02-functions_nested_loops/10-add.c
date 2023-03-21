cabf12b2353a0a577c8f45ea2597101502307b08
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 23:04:56 +0000

    updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code.
+ * @a: Description of parameter x
+ * @b: Description of parameter x
+ *
+ * Description: ddzqdqzd
+ * Description
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
