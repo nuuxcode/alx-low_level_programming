0fc532f1115b0e1a080abf6c71f2276f1aa6e462
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 22:23:00 +0000

    updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_last_digit - dqzdqzdqzdqzd
- * @n: Description of parameter x
+ * jack_bauer - dqzdqzdqzdqzd
  *
  * Description: ddzqdqzd
  * section header: Section description)*
@@ -11,28 +10,27 @@
  */
 void jack_bauer(void)
 {
-	int min_a;
-	int min_b;
+	int ma;
+	int mb;
 	int sec_a;
 	int sec_b;
 
-	for (min_a = 0; min_a < 3; ++min_a)
+	for (ma = 0; ma < 3; ++ma)
 	{
-		for (min_b = 0; min_b < 10 && ((min_a == 2 && min_b < 4) || min_a < 2); ++min_b)
+		for (mb = 0; mb < 10 && ((ma == 2 && mb < 4) || ma < 2); ++mb)
 		{
 			for (sec_a = 0; sec_a < 6; ++sec_a)
 			{
 				for (sec_b = 0; sec_b < 10; ++sec_b)
 				{
-					_putchar(min_a + '0');
-					_putchar(min_b + '0');
+					_putchar(ma + '0');
+					_putchar(mb + '0');
 					_putchar(':');
 					_putchar(sec_a + '0');
 					_putchar(sec_b + '0');
 					_putchar('\n');
 				}
-			
 			}
-		}	
+		}
 	}
 }
