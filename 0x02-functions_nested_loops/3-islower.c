d9bdefd2aca2d7e57c159ed53e886a76c39c281e
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 17:02:12 +0000

    updated

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: sasasa
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	char str;
+	int i = 0;
+
+	if (c >= 65 && c <=  90)
+		printf("Character is Upper Case Letters");
+	else
+		printf("Character is Not Upper Case Letters");
+	return (0);
+}
