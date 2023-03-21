648827243994d062c4308789026f0d3c6baece7f
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 17:47:12 +0000

    updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - dqzdqzdqzdqzd
+ * @c: Description of parameter x
+ *
+ * Description: ddzqdqzd
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <=  90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
