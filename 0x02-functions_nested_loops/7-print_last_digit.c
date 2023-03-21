82c00454c8310e7ff1a3e50f45315db52e82e3b7
Mounssif BOUHLAOUI <you@example.com>
2023-03-21 21:00:44 +0000

    updated

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - dqzdqzdqzdqzd
+ * @n: Description of parameter x
+ *
+ * Description: ddzqdqzd
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
+}
