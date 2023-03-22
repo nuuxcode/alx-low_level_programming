c0ed0290911fc13d5940112c912a4d2e5dad2e88
Mounssif BOUHLAOUI <you@example.com>
2023-03-22 01:32:33 +0000

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,50 @@
 #include "main.h"
 #include "stdio.h"
 /**
- * times_table - check the code.
+ * print_times_table - check the code.
+ * @x: Description of parameter x
  * Description
  * Return: Always 0.
  */
 void print_times_table(int x)
 {
-	if (x < 15 && x > 0)
-	{
-		int i, j, n;
+	int i, j, n;
 
-		for (i = 0; i <= x; i++)
+	for (i = 0; i <= x && (x < 15 && x > 0); i++)
+	{
+		for (j = 0; j <= x; j++)
 		{
-			for (j = 0; j <= x; j++)
+			n = i * j;
+			if (n > 9)
 			{
-				n = i * j;
-				if (n > 9)
+				if (n > 99)
 				{
-					if (n > 99)
-					{
-						_putchar((n / 100) + '0');
-					}
-					_putchar((n / 10) % 10 + '0');
-					_putchar(n % 10 + '0');
+					_putchar((n / 100) + '0');
 				}
-				else
+				_putchar((n / 10) % 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				if (j != 0)
 				{
-					if (j != 0)
+					_putchar(' ');
+				}
+				_putchar(n + '0');
+			}
+			if (j < x)
+			{
+				_putchar(',');
+				if (n / 100 == 0  || n >= 99)
+				{
+					_putchar(' ');
+					if ((i * (j + 1)) / 100 == 0)
 					{
 						_putchar(' ');
-					}
-					_putchar(n + '0');
-				}
-				if (j < x)
-				{
-					_putchar(',');
-					if (n / 100 == 0  || n >= 99)
-					{
-						_putchar(' ');
-						if ((i * (j+1)) / 100 == 0)
-						{
-							_putchar(' ');
-						}
 					}
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
