46f3fc81517267c4af180090cf8a92777e7c0c62
Mounssif BOUHLAOUI <you@example.com>
2023-03-22 02:39:41 +0000

    updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,12 +14,16 @@ int main(void)
 
 	printf("%ld, ", a);
 	printf("%ld, ", b);
-	for (i = 0; i < 48; i++)
+	for (i = 0; i <= 47; i++)
 	{
 		sum = a + b;
 		a = b;
 		b = sum;
-		printf("%ld, ", sum);
+		printf("%ld", sum);
+		if (i != 47)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 	return (0);
