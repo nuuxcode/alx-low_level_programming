2b9b0f5f199ec1370a5c2ad0b044871d06405216
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-23 22:12:44 +0000

    updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - dqzdqzdqzdqzd
- * @c: Description of parameter x
+ * _isupper - entry point
+ * @c: character
  *
- * Description: ddzqdqzd
- * section header: Section description)*
- * Return: Description of the returned value
+ * Description: checks for uppercase character
+ * Return: 1 if c is uppercase 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -10,9 +10,9 @@ int main(void)
 {
 	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
