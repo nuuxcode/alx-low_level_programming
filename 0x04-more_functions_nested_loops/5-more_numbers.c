33ca446e6f28253d6d1a1469ba26c0a2ba9d01e2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-23 22:58:41 +0000

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,19 @@ void more_numbers(void)
 {
 	char n;
 	int i = 0;
-
+	int j = 0;
 	while (i < 10)
 	{
-		for (n = '0'; n <= '9'; n++)
+		n = '0';
+		for (j = 0; j <= 14; j++)
 		{
+			/* 0 1 2 3 4 5 6 7 8 9 (n = 0) 0 1 2 3 4*/
+			if (j > 9)
+				_putchar('1');
+			if (n == 58)
+				n = '0';
 			_putchar(n);
-		}
-		for (n = '0'; n <= '4'; n++)
-		{
-			_putchar('1');
-			_putchar(n);
+			n++;
 		}
 		i++;
 		_putchar('\n');
