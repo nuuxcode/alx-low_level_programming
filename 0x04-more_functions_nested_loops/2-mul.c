0f5369637eb3ce654ad1ce3a214876fdb524fe23
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-23 22:18:56 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: a is a digit
+ * @b: b is a digit
+ *
+ * Description: multiplies two integers.
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
