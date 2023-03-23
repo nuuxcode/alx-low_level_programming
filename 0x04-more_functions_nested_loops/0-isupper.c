356d46fc4c560c04ae686d862604b0afc2cc5b33
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-23 18:35:28 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - dqzdqzdqzdqzd
+ * @c: Description of parameter x
+ *
+ * Description: ddzqdqzd
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
