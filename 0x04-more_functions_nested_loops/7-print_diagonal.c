1745d7ab49fa261bce4b31b198d8e7c12a4fb78c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-24 00:39:56 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ *	@n: int number
+ * Description: --
+ * Return: --
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+	
+	while (i < n)
+	{
+		if (n > 0)
+		{
+			j = 0;
+			while (j <= i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+		i++;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
