44050d13b84fdfb04dd9db1d606aa34d0880525e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-24 01:09:40 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ *	@size: int number
+ * Description: --
+ * Return: --
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
