7e352f87af16220b8eb3c65192b06e7dbbcbb892
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-24 02:45:03 +0000

    updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - check the code
- * @n : dec
- * Return: Always 0.
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
+
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 	if (n < 0)
 	{
-		n = n * -1;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
-
-	if (n / 10)
-	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
