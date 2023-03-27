bf904ced5e02d5fd08a3b0f120696fdea34b2ce1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-27 23:47:14 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: variable
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
 
 #endif /* MAIN_H */
