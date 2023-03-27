05c1e25a420d16bde00ae6a581ee456d3da327d1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-27 22:10:54 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ * @n: size of triangle
+ * Description: --
+ * Return: --
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
