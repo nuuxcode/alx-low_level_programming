6e7f80fc724274dacb47402ded54c06097d9b1ee
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 01:54:55 +0000

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (count - 1) / 2; i <= count - 1; i++)
+		for (i = count / 2; i <= count - 1; i++)
 		{
 			_putchar(str[i]);
 			if (str[i + 1] == '\0')
