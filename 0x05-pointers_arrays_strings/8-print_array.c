07986061608d89fd71c5745f05ff9e2737b02d8d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 02:10:55 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - check the code
+ * @a: variable
+ * @n: variable
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
