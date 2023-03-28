71dcc48206c318895287d49cb435b87590e40de6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 01:23:29 +0000

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - reverses a string
- * @str: string to reverse
+ * puts2 - check the code
+ * @str: variable
+ * Return: void.
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(*(str + i));
+		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+			break;
 	}
 	_putchar('\n');
 }
