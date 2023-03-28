0faf5ceabe03c09bb46986b7e80fd6b0a2bf24f5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 00:03:21 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: variable
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif /* MAIN_H */
