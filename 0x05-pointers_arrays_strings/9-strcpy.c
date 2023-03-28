fa0ac8cf61d472940cd916b478d65c6bd3dd8a0e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 02:32:05 +0000

    updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = -1;
 
-	for (i = 0; src[i] >= '\0'; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
 	return (dest);
 }
