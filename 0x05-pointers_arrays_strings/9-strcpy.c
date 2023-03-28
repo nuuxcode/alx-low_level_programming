c553d212cabc794531df292e6a341991308a1004
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 02:30:18 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - check the code
+ * @dest: pointer
+ * @src: pointer
+ * Return: void.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] >= '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
