76f27e8e0e6d511943197599f321c112b0d3ce6a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-28 01:41:53 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: variable
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int count;
+	int i;
+
+	count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; i <= count; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = count - 1 / 2; i <= count; i--)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
