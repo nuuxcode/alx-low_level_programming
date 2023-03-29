7fdca0f04105e98848e501f385b13dfc598458a0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-29 17:28:11 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * @n: destination string
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
