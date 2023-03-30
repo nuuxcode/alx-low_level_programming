3545cf791f8cad72b9d53ff892ecc20a8e2d5264
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-30 17:04:53 +0000

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int ret;
+
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		ret = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	return (ret);
+	return (0);
 }
