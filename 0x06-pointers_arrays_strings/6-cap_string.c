ad3ed34c2776481f34c7ae457a27d82604d65257
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-31 00:55:58 +0000

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,8 +14,13 @@ char *cap_string(char *str)
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == ',' ||
 			str[i] == ';' || str[i] == '!' || str[i] == '?' ||
 			str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-			str[i] == '{' || str[i] == '}' || str[i] == '\n')
+			str[i] == '{' || str[i] == '}' || str[i] == '\n' ||
+			str[i] == '.')
 		{
+			if (str[i] == '\t')
+			{
+				str[i] = ' ';
+			}
 			if (str[i + 1] <= 'z' && str[i + 1] >= 'a')
 			{
 				str[i + 1] = (str[i + 1] - 'a') + 'A';
