e1495f4d310f87a4ede6d1a4ef72f06ec9a69912
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-31 01:21:08 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - compares two strings.
+ * @str: source string
+ * Return: pointer to the resulting string dest
+ */
+char *leet(char *str)
+{
+	int i, j;
+	int a[] = {'a', 'e', 'o', 't', 'l'};
+	int b[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == a[j] || str[i] == a[j] + 32)
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return (str);
+}
