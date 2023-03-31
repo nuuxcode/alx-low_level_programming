0a3540dfff15e070d9cc2783674d94321d989044
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-03-31 00:51:59 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - compares two strings.
+ * @str: source string
+ * Return: pointer to the resulting string dest
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == ',' ||
+			str[i] == ';' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}' || str[i] == '\n')
+		{
+			if (str[i + 1] <= 'z' && str[i + 1] >= 'a')
+			{
+				str[i + 1] = (str[i + 1] - 'a') + 'A';
+			}
+		}
+	}
+	return (str);
+}
