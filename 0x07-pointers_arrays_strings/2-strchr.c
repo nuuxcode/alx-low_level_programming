17dfa283957781d32a346d02281aa1ee2897cd09
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-03 17:22:30 +0000

    updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			s = (s + i);
-			return (s);
+			return (s + i);
 		}
 	}
 	return (NULL);
