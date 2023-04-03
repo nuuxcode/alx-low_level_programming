a65dd98b99247896d9741ea85b4e0d9197427888
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-03 15:18:20 +0000

    Add memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,12 @@
  * @s: first bytes of the memory
  * @b: constant byte b
  * @n:  bytes of the memory area pointed to by s
- * Return: pointer to the resulting string dest
+ * Return: pointer to the resulting string dests
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
