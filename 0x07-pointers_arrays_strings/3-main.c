e3f9d7b5b779a4868c6b7deb3be04a3defff6c86
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-03 22:05:02 +0000

    updated

--- 0x07-pointers_arrays_strings/3-main.c
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -9,7 +9,7 @@
 int main(void)
 {
     char *s = "hello, world";
-    char *f = "oleh";
+    char *f = "\0";
     unsigned int n;
 
     n = _strspn(s, f);
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,6 +13,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (accept[i] == '\0')
+			break;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
