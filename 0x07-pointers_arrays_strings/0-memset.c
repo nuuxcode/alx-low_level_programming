968cf18a0d0e7a1b8a3c258b8ee9c3b708b54039
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-03 15:20:14 +0000

    Fix betty on memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,14 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-
-/*
-
-Write a function that fills memory with a constant byte.
-
-Prototype: char *_memset(char *s, char b, unsigned int n);
-The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
-Returns a pointer to the memory area s
-FYI: The standard library provides a similar function: memset. Run man memset to learn more.
-
-*/
