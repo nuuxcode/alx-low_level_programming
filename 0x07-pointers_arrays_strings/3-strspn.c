91d095e3d3d29cc6aecb5a2232f47fae77716838
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-03 19:00:14 +0000

    add strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * @accept: constant byte b
+ * Return: pointer to the resulting string dests
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (j == 4)
+			break;
+		}
+	return (i);
+}
