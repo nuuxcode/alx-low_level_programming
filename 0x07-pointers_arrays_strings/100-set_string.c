a71cb6aa1586891641a6582ecb681ffd779387cc
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-04 02:51:10 +0000

    updated

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * @to: first bytes of the memory
+ * Return: -
+ */
+void set_string(char **s, char *to)
+{
+	**s = *to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
