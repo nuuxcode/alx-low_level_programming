c355e7378828e9f090135728cba9c47e3cd1af3e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-05 17:06:32 +0000

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
