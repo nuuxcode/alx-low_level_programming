5cc5bbe1fdc94ea11121b031d8d7ae6d4180673b
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-05 17:52:41 +0000

    add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * Return: -
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s+1)+1);
+}
