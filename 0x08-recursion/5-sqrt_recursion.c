401daee47cfd819703e92a3823fbc645490803e0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-06 01:09:39 +0000

    add 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion2 - fills memory with a constant byte.
+ * @n: first bytes of the memory
+ * @sqrt: first bytes of the memory
+ * @res: first bytes of the memory
+ * Return: -
+ */
+float _sqrt_recursion2(int n, float sqrt, float res)
+{
+	if (sqrt == res)
+	{
+		int int_sqrt = (int)sqrt;
+
+		if ((float)int_sqrt == sqrt)
+		{
+			return (sqrt);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	res = sqrt;
+	sqrt = (n / res + res) / 2;
+	return (_sqrt_recursion2(n, sqrt, res));
+}
+
+/**
+ * _sqrt_recursion - fills memory with a constant byte.
+ * @n: first bytes of the memory
+ * Return: -
+ */
+int _sqrt_recursion(int n)
+{
+	float res, sqrt;
+
+	sqrt = n / 2;
+	res = 0;
+
+	if (n < 0)
+		return (-1);
+	return (_sqrt_recursion2(n, sqrt, res));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_prime_number - fills memory with a constant byte.
+ * @n: first bytes of the memory
+ * Return: -
+ */
+int is_prime_number(int n);
+{
+	
+}
+
