e4475ad57b11b5f3b45deb80944176bd2ad5e0b6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-10 23:07:53 +0000

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,35 +12,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum, i;
-	long unsigned int j;
+	int sum, i,j;
 
 	sum = 0;
 	if (argc != 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			/*printf("%d %s\n", atoi(argv[i]), argv[i]);*/
-			for (j = 0; j < strlen(argv[i]); j++)
+			for (j = 0; j < (int)strlen(argv[i]); j++)
 			{
 				if (!isdigit(argv[i][j]))
 				{
-					/*printf("'%c' is not a number\n", argv[i][j]);*/
 					printf("Error\n");
 					return (1);
-					/*break;*/
 				}
 			}
 			sum += atoi(argv[i]);
-			/*if (atoi(argv[i]) <= 9 && atoi(argv[i]) >= 1)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}*/
 		}
 	}
 	printf("%d\n", sum);
