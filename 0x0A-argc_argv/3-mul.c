a5a147e38c6479b25d0bf5e7cf32898462ab91a0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-10 22:14:28 +0000

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,11 +18,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
 	return (0);
 }
