88443184d69f2f62dfed60464019742ce6448b45
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-10 22:10:14 +0000

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	else
+	{
+		printf("%d\n", a * b);
+	}
+	return (1);
+}
