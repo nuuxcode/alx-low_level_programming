082c391dee02a38c9f0fb22d61b9b83872fe3cca
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-10 21:32:27 +0000

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
