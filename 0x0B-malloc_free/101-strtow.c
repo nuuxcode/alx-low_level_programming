854d37f48a105a5b09b2e79ee870213e2266cebb
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-12 02:58:40 +0000

    updated

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * strtow - function that concatenates two strings
+ * @str: first bytes of the memory
+ * Return: pointer to the resulting string dests
+ */
+
+char **strtow(char *str)
+{
+}
