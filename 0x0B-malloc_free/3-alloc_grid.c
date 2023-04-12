33052a77eb5b11cdf7772a219f906bb32b192e44
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-12 01:30:38 +0000

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,11 +8,18 @@
 int **alloc_grid(int width, int height)
 {
 	int i;
-
 	int **grid = (int **)malloc(height * sizeof(int *));
+	if (grid)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
+		if (!grid[i])
+		{
+			return (NULL);
+		}
 	}
 
 	return (grid);
