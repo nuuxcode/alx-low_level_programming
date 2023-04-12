f7b6a6c6f6e071930403e7d82382c5f567d7fe45
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-12 01:32:56 +0000

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,6 +9,7 @@ int **alloc_grid(int width, int height)
 {
 	int i;
 	int **grid = (int **)malloc(height * sizeof(int *));
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
