e12c9b220453d6ac6348ccacf3333f8f8ab8bd5d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-13 15:12:54 +0000

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 	char *copy;
 	unsigned int size1 = 0;
+	unsigned int size2 = 0;
 
 	if (s2 == NULL)
 		s2 = "";
@@ -21,16 +22,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		size1++;
 	}
-	copy = malloc((size1 + n + 1) * sizeof(*copy));
+	while (s2[size2])
+	{
+		size2++;
+	}
+	size2 = size2 - (size2 - n);
+	copy = malloc(size1 + size2 + 1);
 	if (!copy)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size1; i++)
-		copy[i] = s1[i];
-
-	for (i = 0; i < n; i++)
-		copy[size1 + i] = s2[i];
-	copy[size1 + n] = '\0';
+	for (i = 0; i < size1 + size2; i++)
+	{
+		if (i < size1)
+		{
+			copy[i] = s1[i];
+		}
+		else
+		{
+			copy[i] = s2[i - size1];
+		}
+	}
 	return (copy);
 }
