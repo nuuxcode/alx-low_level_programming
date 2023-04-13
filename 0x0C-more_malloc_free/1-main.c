15a292aa94b876b6abda1d0ec771b4295293dc6f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-13 13:54:18 +0000

    updated

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,10 +11,10 @@ int main(void)
 {
     int i;
     char *concat;
-    concat = string_nconcat("Best ", "School !!!", 11);
+    concat = string_nconcat("Best ", "aaa", 5);
     printf("%s\n", concat);
 
-    for (i = 0; i < (int)strlen(concat); i++)
+    for (i = 0; i < (int)strlen(concat)+1; i++)
     {
         printf("%02x ", concat[i]);
     }
