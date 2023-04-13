a12fa7e4c07847c16d869cc7f91069adef537f5e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-13 21:41:17 +0000

    updated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,11 +10,10 @@ int *array_range(int min, int max)
 {
 	int *s;
 	int i;
-	int size;
+
 	if (min > max)
 		return (NULL);
-	size = max - min;
-	s = malloc(size);
+	s = malloc((max - min + 1) * sizeof(int));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i <= max - min; i++)
