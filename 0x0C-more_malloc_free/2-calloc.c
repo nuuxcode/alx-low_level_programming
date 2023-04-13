9ef7b7a52e7d6c0adc376470e99e55edcd152148
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-13 21:25:06 +0000

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,9 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * array_range - function that creates an array of chars,
+ *  and initializes it with a specific char
+ * @min: first bytes of the memory
+ * @max: first bytes of the memory
+ * Return: pointer to the resulting string dests
+ */
+int *array_range(int min, int max)
+{
+	int *s;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	s = malloc(max - min);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i <= max - min; i++)
+	{
+		s[i] = min+i;
+	}
+	return (s);
+}
