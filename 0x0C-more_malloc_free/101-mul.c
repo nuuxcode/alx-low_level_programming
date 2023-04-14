869b1f95d89805bb71434c441e3902a1bff63b26
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-14 02:50:09 +0000

    updated

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints buffer in hexa
+ * @argc: the address of memory to print
+ * @argv: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char **argv)
+{
+
+
+	return (0);
+}
