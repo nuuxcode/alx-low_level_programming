776318ba67e68e970888238c0af1d1d0b18e5f6c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-14 02:42:47 +0000

    updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size * sizeof(char));
 		return (ptr);
 	}
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
