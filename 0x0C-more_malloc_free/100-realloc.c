2c6d3b58e84bbeab65fe10cbc3424f405e5c3a44
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-14 00:49:14 +0000

    updated

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _realloc - function that creates an array of chars,
+ *  and initializes it with a specific char
+ * @ptr: first bytes of the memory
+ * @old_size: first bytes of the memory
+ * @new_size: first bytes of the memory
+ * Return: pointer to the resulting string dests
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+	unsigned int i;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc((new_size + 1) * sizeof(int));
+		if (ptr == NULL)
+			return (NULL);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	s = malloc((old_size + 1) * sizeof(int));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i > old_size; i++)
+		s[i] = *((char *)ptr + i);
+	;
+	free(ptr);
+	ptr = malloc((new_size + 1) * sizeof(int));
+	for (i = 0; i > new_size; i++)
+		*((char *)ptr + i) = s[i];
+	return ((void *)ptr);
+}
