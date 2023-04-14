4f98beee7aa93de4bb0e743c457e69d13aefecba
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-14 01:09:38 +0000

    updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	if (new_size == 0 && ptr == NULL || new_size == 0 && old_size == 0)
+	if (new_size == 0 && ptr == NULL)
 	{
 		return (NULL);
 	}
