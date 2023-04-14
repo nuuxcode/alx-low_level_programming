401c9bc59512ddde36951442b2e2cc7c9780f891
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-14 02:46:06 +0000

    updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	for (i = 0; i < old_size && new_size > old_size; i++)
 		s[i] = *((char *)ptr + i);
-	for (i = 0; i < new_size && new_size <= old_size; i++)
+	for (i = 0; i < new_size && new_size < old_size; i++)
 		s[i] = *((char *)ptr + i);
+	free(ptr);
 	return (s);
 }
