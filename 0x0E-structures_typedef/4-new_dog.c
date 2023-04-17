2cfa60a464e162ac9c6d55951590d0307a17eb7e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 22:08:59 +0000

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,6 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (ownerL + 1));
 	if (dog->owner == NULL)
 	{
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
