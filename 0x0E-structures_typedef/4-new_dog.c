cf4e4fa836516a83e8d43201ca08c1165dcbb3c3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 21:54:03 +0000

    updated

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - entry point
+ * @name: size of triangle
+ * @age: size of triangle
+ * @owner: size of triangle
+ * Description: --
+ * Return: --
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dog;
+	dog = malloc(sizeof(struct dog));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
