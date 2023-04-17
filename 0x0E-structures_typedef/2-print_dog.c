1c7af361c62ffeb11bff8991adfb554a28b8fc5c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 21:04:27 +0000

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n",d->name);
-	printf("Age: %f\n",d->age);
-	printf("Owner: %s\n",d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
