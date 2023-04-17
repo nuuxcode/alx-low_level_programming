e2d1dea7c91622f340b7f41c480f43044482367c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 20:53:29 +0000

    updated

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    init_dog(NULL, "Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
