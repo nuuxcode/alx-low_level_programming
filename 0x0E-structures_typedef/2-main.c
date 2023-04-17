9e17b4ce989507474185ace7e7fe7ffc92a3039a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 21:17:25 +0000

    updated

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     struct dog my_dog;
 
-    my_dog.name = "Poppy";
+    my_dog.name = NULL;
     my_dog.age = 3.5;
-    my_dog.owner = "Bob";
+    my_dog.owner = NULL;
     print_dog(&my_dog);
     return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
