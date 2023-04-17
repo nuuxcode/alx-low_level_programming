9341cb0192bb143855848b84c8f2a87888375076
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 20:31:10 +0000

    updated

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - entry point
+ * @d: size of triangle
+ * @name: size of triangle
+ * @age: size of triangle
+ * @owner: size of triangle
+ * Description: --
+ * Return: --
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,5 @@ struct dog
 	char *owner;
 	float age;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
