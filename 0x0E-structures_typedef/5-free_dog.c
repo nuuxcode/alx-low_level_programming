e9bfca65f3b72acfb90ad82581cb5d82087acdb1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 23:56:41 +0000

    updated

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,9 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
