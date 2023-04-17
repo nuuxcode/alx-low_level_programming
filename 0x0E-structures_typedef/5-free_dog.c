33029950f90720821da7c28e3e8c30d9b096674c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 23:40:56 +0000

    updated

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - entry point
+ * @d: size of triangle
+ * Description: --
+ * Return: --
+ */
+void free_dog(dog_t *d);
+{
+	free(d->owner);
+	free(d->age);
+	free(d->name);
+	free(d);
+}
