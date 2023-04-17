c98b1c75ed32cba9c5688549f5856848e98b8f7c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-17 22:08:16 +0000

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int i;
+	int ownerL = 0;
+	int nameL = 0;
 
+	while (name[nameL])
+		nameL++;
+	while (owner[ownerL])
+		ownerL++;
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+	dog->name = malloc(sizeof(char) * (nameL + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	for (i = 0; name[i] != '\0'; i++)
 	{
 		dog->name[i] = name[i];
 	}
 	dog->name[i] = '\0';
+	dog->owner = malloc(sizeof(char) * (ownerL + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	for (i = 0; owner[i] != '\0'; i++)
 	{
 		dog->owner[i] = owner[i];
