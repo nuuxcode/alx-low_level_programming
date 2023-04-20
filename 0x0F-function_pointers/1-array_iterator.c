73c095315a7fad00287e6c4a89f2dd3347829601
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 02:28:28 +0000

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+		return;
 	while (i < (int)size)
 	{
 		action(array[i]);
