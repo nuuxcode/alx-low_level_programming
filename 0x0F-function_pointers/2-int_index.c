7b43c5e61d8672dcc4da6759cf3c28a6970bfae9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 02:51:03 +0000

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 	while (i < (int)size && size > 0)
 	{
