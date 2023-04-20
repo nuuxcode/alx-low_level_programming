3c3136516daeec2f22187860cd109b781832fad5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 19:16:21 +0000

    add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - entry point
+ * @separator: size of triangle
+ * @n: size of triangle
+ * Description: --
+ * Return: --
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i = 0;
+	va_start(args, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		printf("%d%s", va_arg(args, int), separator);
+	}
+	printf("\n");
+}
