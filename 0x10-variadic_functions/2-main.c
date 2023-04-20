4492bd3ead7f245cb7361a281311e00248beca5a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 20:47:43 +0000

    updated

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -11,5 +11,8 @@ int main(void)
     print_strings(", ", 2, "Jay", "Django");
     print_strings("", 2, "Jay", "Django");
     print_strings(NULL, 2, "Jay", "Django");
+    print_strings(", ", 2, NULL, "Django");
+    print_strings("", 2, "Jay", NULL);
+    print_strings(NULL, 2, NULL, NULL);
     return (0);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,11 +11,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	int i = 0;
-
+	char *s;
 	va_start(args, n);
 	for (i = 0; i < (int)n; i++)
 	{
-		printf("%s%s", va_arg(args, char*),
+		s = va_arg(args, char *);
+		printf("%s%s",
+			   s == NULL ? "(nil)" : s,
 			   i != (int)n - 1 && separator != NULL ? separator : "");
 	}
 	printf("\n");
