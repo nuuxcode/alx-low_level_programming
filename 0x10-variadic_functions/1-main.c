02611e41d7e16ab0d2d26dcf9bf7deed9c2aeeb2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 19:22:24 +0000

    updated

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -10,5 +10,6 @@ int main(void)
     print_numbers(", ", 4, 0, 98, -1024, 402);
     print_numbers("", 4, 0, 98, -1024, 402);
     print_numbers(NULL, 4, 0, 98, -1024, 402);
+    print_numbers(NULL,0);
     return (0);
 }
