94e37bed67457680788e9242c347622008a4cab7
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-20 01:55:10 +0000

    updated

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - entry point
+ * @name: size of triangle
+ * @f: size of triangle
+ * Description: --
+ * Return: --
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef function_pointers
+#define function_pointers
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
