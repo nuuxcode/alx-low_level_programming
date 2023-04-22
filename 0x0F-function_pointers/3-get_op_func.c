e32a00d436e181939e00ac212f9c4ed9f94bada1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-22 00:19:08 +0000

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,7 +16,7 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}};
     int i = 0;
-    while (i < 4)
+    while (ops[i].op != NULL)
     {
         if (*s == *ops[i].op)
         {
