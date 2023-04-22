326c487fdc9f28a443fadcb63a23d7807c4f8895
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-22 01:05:33 +0000

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
 	char *s = argv[2];
 
 	if (argc != 4)
@@ -25,11 +23,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (b == 0 && (s[0] == '/' || s[0] == '%'))
+	if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(a, b));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
