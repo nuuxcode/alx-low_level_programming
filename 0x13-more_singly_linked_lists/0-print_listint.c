ded1b28eb1963e2ee21828b2b1009502cbb6ae2c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-28 15:42:46 +0000

    add 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - check the code
+ * @h: a
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *nav = h;
+	size_t count = 0;
+
+	while (nav)
+	{
+		printf("%d\n", nav->n);
+		count++;
+		nav = nav->next;
+	}
+	return (count);
+}
