631d3780ad6036aa45eb3d30ce240ea6411c215e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-28 16:38:29 +0000

    add 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -27,3 +27,12 @@ int main(void)
     free(new);
     return (0);
 }
+
+/*
+julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_listint.c -o a
+julien@ubuntu:~/0x13. More singly linked lists$ ./a
+9
+8
+-> 2 elements
+julien@ubuntu:~/0x13. More singly linked lists$
+*/
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - check the code
+ * @head: a
+ * @n: a
+ * Return: Always 0.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
