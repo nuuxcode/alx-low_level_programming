8dd2083a8b72c6027b3e9e7c8e0c7ac708b9267a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-30 16:30:13 +0000

    debuging

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -22,17 +25,8 @@ int main(void)
     new->n = 9;
     new->next = head;
     head = new;
-    n = print_listint(NULL);
+    n = print_listint(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (0);
 }
-
-/*
-julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_listint.c -o a
-julien@ubuntu:~/0x13. More singly linked lists$ ./a
-9
-8
--> 2 elements
-julien@ubuntu:~/0x13. More singly linked lists$
-*/
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
-#include <string.h>
+
 #include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,6 +17,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
