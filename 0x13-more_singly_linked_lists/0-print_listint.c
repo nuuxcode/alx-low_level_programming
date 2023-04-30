4c53ef6e84ccfbdaa8a6e66232020cdb8a87875f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-30 16:35:51 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *nav = h;
 	size_t count = 0;
-	if (h == NULL)
-		return (0);
-	while (nav)
+	while (h)
 	{
-		printf("%d\n", nav->n);
+		printf("%d\n", h->n);
 		count++;
-		nav = nav->next;
+		h = h->next;
 	}
 	return (count);
 }
