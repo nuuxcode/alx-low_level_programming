c150e8008e46ddeb59eb91a6767ac7866fbcd080
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-04-30 20:58:18 +0000

    updated

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,6 +10,8 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int data = (*head)->n;
 
+	if (head == NULL || *head == NULL)
+		return (0);
 	tmp = *head;
 	*head = (*head)->next;
 	free(tmp);
