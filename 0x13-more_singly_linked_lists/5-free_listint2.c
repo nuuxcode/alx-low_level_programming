1fc6def04590087529acecae4268f467ce9a60b2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-02 19:05:49 +0000

    updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,12 +2,12 @@
 /**
  * free_listint2 - Frees a list
  * @head: Adress of the pointer to head
-*/
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *nex, *tofree;
 
-	if (!*head || !head)
+	if (!head || !*head)
 		return;
 	tofree = *head;
 	while (tofree)
