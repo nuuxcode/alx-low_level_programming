e185d95d6cdff9b4f5df0d71f703b0fcb5133143
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-02 19:19:08 +0000

    updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * pop_listint - check the code
+ * get_nodeint_at_index - check the code
  * @head: a
+ * @index: a
  * Return: Always 0.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
