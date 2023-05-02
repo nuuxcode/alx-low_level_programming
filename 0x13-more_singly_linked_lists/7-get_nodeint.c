b81dcf5f2da258321d6dde0a889f542d06f446d9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-02 19:18:45 +0000

    updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *nav = head;
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
-	while (i >= index)
+	while (i <= index)
 	{
 		if (i == index)
 		{
