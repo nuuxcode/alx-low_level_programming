02c9d5880d291ae6c6a91e9373e9318023b45fe1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-02 18:40:22 +0000

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *nav = *head;
 
-	if (head == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
