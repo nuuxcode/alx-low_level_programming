b65615f2fbcc8bd6d439f4ceb15f9381fa11ed47
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-03 01:10:38 +0000

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (!*head)
+	if (!*head && idx == 0)
 	{
 		*head = new;
 	}
