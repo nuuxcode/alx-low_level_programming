5aee1733294125517569e59ce51a05ea41d98b0d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-03 00:47:09 +0000

    updated

--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -17,7 +17,7 @@ int main(void)
     add_nodeint_end(&head, 1);
     print_listint(head);
     printf("-----------------\n");
-    insert_nodeint_at_index(&head, 3, 4096);
+    insert_nodeint_at_index(&head, -1, 4096);
     print_listint(head);
     free_listint2(&head);
     return (0);
