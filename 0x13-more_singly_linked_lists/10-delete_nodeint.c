1da3fda5431f3ae81ae3c4eb7212a8a2df79a372
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-03 17:27:12 +0000

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,19 +8,22 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *nav, *before;
+	listint_t *nav, *before, *deleted;
 	unsigned int i = 0;
 
-	nav = *head;
 	if (!head || !*head)
 		return (-1);
+	nav = *head;
 	if (index == 0)
 	{
 		*head = nav->next;
+		free(nav);
 		return (1);
 	}
 	while (nav)
 	{
+		if (i == index)
+			deleted = nav;
 		if (i == index - 1)
 		{
 			before = nav;
@@ -28,6 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (i == index + 1)
 		{
 			before->next = nav;
+			free(deleted);
 			return (1);
 		}
 		nav = nav->next;
