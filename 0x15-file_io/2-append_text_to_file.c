4c0e3013a4055634fb250637cc10cf88e5ee98f9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 18:12:01 +0000

    updated

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - function that converts a binary number to an unsigned int
+ * @filename: a
+ * @text_content: a
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	while (text_content && text_content[len])
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
