86006d2ea3f4e346c57cf751dbaf0254b1e6c7ee
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 17:58:39 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,6 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-	{
-		return (-1);
-	}
 	while (text_content && text_content[len])
 		len++;
 	write(fd, text_content, len);
