41284c3812ea2c527e0188e957fbacc8413b30d8
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 16:46:56 +0000

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY,0777);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
