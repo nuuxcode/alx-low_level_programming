6ca787c19462e069d42ddc9197ff1d2fe8f92c98
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 17:50:43 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 	}
-	while (text_content[len])
+	while (text_content && text_content[len])
 		len++;
 	write(fd, text_content, len);
 	if (close(fd) == -1)
--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -10,13 +10,9 @@
 int main(int ac, char **av)
 {
     int res;
+    (void)ac;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
+    res = create_file(av[1], NULL);
     printf("-> %i)\n", res);
     return (0);
 }
