0b7519560857ded62ea249e99068dc8238489256
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 16:58:50 +0000

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,6 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 	ssize_t num_bytes;
+	ssize_t num_writes;
 
 	if (!filename)
 		return (0);
@@ -26,7 +27,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	if (write(STDOUT_FILENO, buffer, letters) == -1)
+	num_writes = write(STDOUT_FILENO, buffer, letters);
+	if (num_writes == -1)
 	{
 		free(buffer);
 		return (0);
