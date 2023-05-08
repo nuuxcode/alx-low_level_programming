02fd9ee83945f31819424fc033a69575d6c1495f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-08 16:38:01 +0000

    updated

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that converts a binary number to an unsigned int
+ * @filename: a
+ * @letters: a
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t num_bytes;
+	ssize_t i;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = (char *)malloc(letters * sizeof(char));
+	num_bytes = read(fd, buffer, letters);
+	for (i = 0; i < num_bytes; i++)
+	{
+		putchar(buffer[i]);
+	}
+	if (num_bytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (num_bytes);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
