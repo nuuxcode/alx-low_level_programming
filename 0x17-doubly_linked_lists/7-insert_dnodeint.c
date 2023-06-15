202a67b018e9c769bd7ff531f89b2b3242c9b8d4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 21:38:45 +0000

    Task7 had to add function, and fix betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
 /**
+ * add_dnodeint_end - adds a new node at the end
+ * @head: head of double list
+ * @n: new node
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/**
+ * add_dnodeint -  adds a new node at the beginning
+ * @head: head of double list
+ * @n: new node
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/**
  * insert_dnodeint_at_index -  inserts a new
  * node at a given position
  * @h: head of double list
@@ -9,8 +23,6 @@
  * Return: address of the new node
  * or NULL if it failed
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *nav, *new, *before;
