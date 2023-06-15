889a3e539e360cfcdbf7c8c547180336d20ed8c3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 18:35:37 +0100

    Task2 fix: betty parentheses

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,5 +20,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
-	return new;
+	return (new);
 }
--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -15,12 +15,6 @@ int main(void)
     head = NULL;
     add_dnodeint(&head, 0);
     add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
     print_dlistint(head);
     return (EXIT_SUCCESS);
 }
