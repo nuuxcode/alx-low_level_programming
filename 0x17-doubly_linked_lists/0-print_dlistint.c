5a4150a52ea8b6d29e13185097971294d101b77a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 16:40:10 +0100

    Task0 fix: betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint -  prints all the elements of a dlistint_t list
+ * @h: head of double list
+ * Return: the number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
