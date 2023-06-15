0c5ba00f1f59728c70325522dbd086e8615ed4c3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 19:01:55 +0100

    Task2 fix: add case where list is empty, next point to NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,11 +11,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (!head)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
+	if (*head)
+		new->next = *head;
+	else
+		new->next = NULL;
 	new->prev = NULL;
 	*head = new;
 	return (new);
