9d8755bdac9e9f210801091ad32d9ed1fcb99640
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 18:29:13 +0100

    Task2 fix: add malloc fail

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -13,7 +13,7 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint(NULL, 0);
+    add_dnodeint(&head, 0);
     add_dnodeint(&head, 1);
     add_dnodeint(&head, 2);
     add_dnodeint(&head, 3);
