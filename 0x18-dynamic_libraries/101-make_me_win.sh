f3c392f97024a39c5dd711bf067167303adc112e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-19 19:36:38 +0000

    Task3 trying another way

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o /tmp/random.so https://raw.githubusercontent.com/nuuxcode/alx-low_level_programming/4d14befd931c55f3c9b867a7db09b52605c656b8/0x18-dynamic_libraries/random.so
+wget https://raw.githubusercontent.com/nuuxcode/alx-low_level_programming/4d14befd931c55f3c9b867a7db09b52605c656b8/0x18-dynamic_libraries/random.so -o /tmp/random.so
 export LD_PRELOAD="/tmp/random.so"
