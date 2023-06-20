beafba0a7790eaf2d9263aa125480d0896b2eaf5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-20 12:24:22 +0000

    Change Random.c to Randomx.c to avoid conflict

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -N -P /tmp https://raw.githubusercontent.com/nuuxcode/alx-low_level_programming/master/0x18-dynamic_libraries/random.so
-export LD_PRELOAD="/tmp/random.so"
+wget -N -P /tmp https://raw.githubusercontent.com/nuuxcode/alx-low_level_programming/master/0x18-dynamic_libraries/randomx.so
+export LD_PRELOAD="/tmp/randomx.so"
