00a44abbbcf26b307d448460373841631d35d7ba
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 06:19:58 +0000

    Task0 fix: add malloc fail case, add case size == 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
 	table = malloc(sizeof(hash_node_t));
+	if (!table)
+		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (!table->array)
