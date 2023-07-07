a170e9b9e29d8859720e9b41fcf9a027138d299a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 18:34:05 +0000

    Task3 testing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,13 +12,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index = 0;
 
-	if (!ht || key[0] == '\0')
+	if (!ht || !key || key[0] == '\0')
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		return (0);
+	}
 	new_node->key = malloc(strlen(key) + 1);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = malloc(strlen(value) + 1);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
 	if (ht->array[index])
