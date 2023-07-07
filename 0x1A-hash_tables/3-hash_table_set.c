3ce88a2bd536b4c7ee73e13d711b77db1ed7c45e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 11:54:05 +0000

    Task3 fix: add malloc error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || key[0] == '\0')
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
+	if(!new_node)
+		return (0);
 	new_node->key = malloc(strlen(key) + 1);
 	new_node->value = malloc(strlen(value) + 1);
 	strcpy(new_node->key, key);
