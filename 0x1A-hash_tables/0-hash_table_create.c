c8fd5de4d14af2283049585068145047c4a692c6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 06:13:14 +0000

    Task0 fix: fix betty add desc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_create - creates a hash table
+ * @size: The size of the array
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
