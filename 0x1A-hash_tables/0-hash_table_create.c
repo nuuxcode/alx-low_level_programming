745781e5c2894d9f06f29c8d2ec7919400a58e02
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 06:32:11 +0000

    Task0 fix: put null on new element

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
 
 	if (size == 0)
 		return (NULL);
-	table = malloc(sizeof(hash_node_t));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (!table->array)
 		return (NULL);
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
