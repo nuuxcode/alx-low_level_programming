7deb7616c2d6de01279f1efcf35395f05ac15f12
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-07 11:08:52 +0000

    Task3 fix: remove printf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 
 	index = key_index((const unsigned char *)key, ht->size);
-	printf("index is : %lu\n", index);
 
 	new_node = malloc(sizeof(hash_node_t));
 	new_node->key = malloc(strlen(key) + 1);
