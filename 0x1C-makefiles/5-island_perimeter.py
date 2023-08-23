a7b2e666bcc51e9bb3f0951b041d56878906db51
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-23 12:32:45 -0700

    Task5 Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,16 +22,24 @@ def count_water(grid, row_i, col_i):
         left = grid[row_i][col_i - 1]
         if left == 0:
             counter += 1
+    else:
+        counter += 1
     if col_i < max_i_col:
         right = grid[row_i][col_i + 1]
         if right == 0:
             counter += 1
+    else:
+        counter += 1
     if row_i > 0:
         up = grid[row_i - 1][col_i]
         if up == 0:
             counter += 1
+    else:
+        counter += 1
     if row_i < max_i_row:
         down = grid[row_i + 1][col_i]
         if down == 0:
             counter += 1
+    else:
+        counter += 1
     return counter
